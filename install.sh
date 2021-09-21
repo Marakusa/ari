3792868c2a2800a76b2304e5688c82c2d7eec6d8
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-09-21 13:27:30 +0300

    Fixed filename casing

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo mkdir -m 777 /usr/lib/ari
-sudo cp -fv ARI/ari.py /usr/lib/ari/ari.py
+sudo cp -fv ARI/ARI.py /usr/lib/ari/ari.py
 sudo cp -fv ARI/install.py /usr/lib/ari/install.py
 sudo ln /usr/lib/ari/ari.py /bin/ari
 ./requirements.sh
