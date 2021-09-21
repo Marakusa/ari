eeb03f86e392eff567c3f1cc65049861ad708fd8
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-09-21 13:21:56 +0300

    Update install

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-cp ./ARI/ari.py /usr/lib/ari/ari.py
-cp ./ARI/install.py /usr/lib/ari/install.py
+cp -fv ARI/ari.py /usr/lib/ari/ari.py
+cp -fv ARI/install.py /usr/lib/ari/install.py
 ln /usr/lib/ari/ari.py /bin/ari
 ./requirements.sh
