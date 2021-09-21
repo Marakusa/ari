f212418933a043d59a1b1191244bfad6f22b6e22
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-09-21 14:01:04 +0300

    Updated shortcut

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ echo ""
 
 echo "Creating the binaries..."
 echo "==> '/bin/ari'"
-sudo echo "python /usr/lib/ari/ari.py $1 $2" >> /bin/ari
+sudo cp -fv ARI/ari /bin/ari
 
 echo "Done! (3/5)"
 echo ""
