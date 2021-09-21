3a504fb35cbabed6cd35b65282d22ca5fbe082e6
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-09-21 13:24:51 +0300

    Added mkdir to install

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-cp -fv ARI/ari.py /usr/lib/ari/ari.py
-cp -fv ARI/install.py /usr/lib/ari/install.py
-ln /usr/lib/ari/ari.py /bin/ari
+sudo mkdir -m 777 /usr/lib/ari
+sudo cp -fv ARI/ari.py /usr/lib/ari/ari.py
+sudo cp -fv ARI/install.py /usr/lib/ari/install.py
+sudo ln /usr/lib/ari/ari.py /bin/ari
 ./requirements.sh
