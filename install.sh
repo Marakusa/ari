86e2ef1c577c91ab99f539ac7d4126bef136373b
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-09-21 13:29:20 +0300

    Fixed requirements path in install

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@ sudo mkdir -m 777 /usr/lib/ari
 sudo cp -fv ARI/ARI.py /usr/lib/ari/ari.py
 sudo cp -fv ARI/install.py /usr/lib/ari/install.py
 sudo ln /usr/lib/ari/ari.py /bin/ari
-./requirements.sh
+./ARI/requirements.sh
