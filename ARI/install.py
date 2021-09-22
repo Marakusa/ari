48f1dbefd4810c69fe8c21af9172d01f1765b58a
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-09-22 09:09:54 +0300

    Updated install script

--- ARI/install.py
+++ ARI/install.py
@@ -11,19 +11,19 @@ def install(query):
 	packageurl = packageFormat.format(query);
 	response = requests.get(packageurl)
 	if response.status_code == 200:
-		print("git clone " + giturl)
-		subprocess.run(["git", "clone", giturl, "/tmp/ari"])
+		print("git clone " + giturl + " /tmp/ari/" + query)
+		subprocess.run(["git", "clone", giturl, "/tmp/ari/" + query])
 
-		print(["cd /tmp/ari/" + query + "/"])
+		print("cd /tmp/ari/" + query + "/")
 		subprocess.run(["cd", "/tmp/ari/" + query + "/"])
 
-		print(["makepkg -si"])
+		print("makepkg -si")
 		subprocess.run(["makepkg", "-si"])
 
-		print(["cd .."])
+		print("cd ..")
 		subprocess.run(["cd", ".."])
 
-		print(["rm -rf " + query + "/"])
+		print("rm -rf " + query + "/")
 		subprocess.run(["rm", "-rf", query + "/"])
 	else:
 		print(f"An error occurred (HTTP Status Code: {response.status_code} - {url})")
