9b40243aff95cd689ccc3bc22816de90135a93ef
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-09-22 11:17:28 +0300

    Updated updater

--- ARI/update.sh
+++ ARI/update.sh
@@ -1,11 +1,12 @@
+currentver=$(wget /usr/lib/ari/version -q -O -)
+
+echo "Current version: " + $currentver
 echo "Checking for updates..."
 
-content=$(wget https://raw.githubusercontent.com/Marakusa/ari/main/ARI/version -q -O -)
-echo $content
-echo diff /usr/lib/ari/version $content
-if [ ! diff /usr/lib/ari/version $content ]
+newver=$(wget https://raw.githubusercontent.com/Marakusa/ari/main/ARI/version -q -O -)
+if [ $currentver != $newver ]
 then
-	echo "A new update available! Downloading..."
+	echo "A new update available (" + $newver + ")! Downloading..."
 	git clone https://github.com/Marakusa/ari.git /tmp/ari/ari
 	sudo chmod a+x /tmp/ari/ari/install.sh
 	bash /tmp/ari/ari/install.sh
