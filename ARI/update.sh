f32c8d44767946ca9fed276db48d7a8648842f65
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-09-22 11:02:35 +0300

    Added updater

---
+++ ARI/update.sh
@@ -0,0 +1,9 @@
+echo "Checking for updates..."
+
+if [ ! diff /usr/lib/ari/version https://raw.githubusercontent.com/Marakusa/ari/main/ARI/version ]
+then
+	echo "A new update available! Downloading..."
+	git clone https://github.com/Marakusa/ari.git /tmp/ari/ari
+	sudo chmod a+x /tmp/ari/ari/install.sh
+	bash /tmp/ari/ari/install.sh
+fi
