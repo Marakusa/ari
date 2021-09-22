1c10eb30a478d0429ff4e19b51009ca38f8a02bf
Marakusa <29477753+Marakusa@users.noreply.github.com>
2021-09-22 11:31:37 +0300

    Fixed version checking

--- ARI/update.sh
+++ ARI/update.sh
@@ -7,14 +7,14 @@ newver=$(wget https://raw.githubusercontent.com/Marakusa/ari/main/ARI/version -q
 
 echo "Latest version: $newver"
 
-if [ currentver != newver ]
+if [[ "$currentver" == "$newver" ]]
 then
+	echo "You have the latest version ($currentver) installed!"
+else
 	echo "A new update available ($newver)! Downloading..."
 	git clone https://github.com/Marakusa/ari.git /tmp/ari/ari
 	cd /tmp/ari/ari
 	sudo chmod a+x install.sh
 	bash ./install.sh
 	sudo rm -rf /tmp/ari/ari
-else
-	echo "You have the latest version ($currentver) installed!"
 fi
